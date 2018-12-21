33aad2105c6080e8560f6462bb77238f65533f73
joostlek <joostlek@outlook.com>
2018-12-21 21:27:59 +0100

    Add Spring Boot
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ src/main/java/nl/hu/tosad/TosadApplication.java
@@ -0,0 +1,17 @@
+package nl.hu.tosad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class TosadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TosadApplication.class, args);
+    }
+
+}
+
