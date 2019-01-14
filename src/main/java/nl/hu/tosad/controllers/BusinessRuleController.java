cde0a958a39de40358ff48019b42d42a9bb82d22
joostlek <joostlek@outlook.com>
2019-01-14 23:04:53 +0100

    Add a basic form of adding a business rule
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/hu/tosad/controllers/BusinessRuleController.java
+++ src/main/java/nl/hu/tosad/controllers/BusinessRuleController.java
@@ -1,19 +1,49 @@
 package nl.hu.tosad.controllers;
 
+import nl.hu.tosad.entities.BusinessRuleBuilder;
+import nl.hu.tosad.entities.BusinessRuleBuilderFactoryInterface;
+import nl.hu.tosad.entities.BusinessRuleTypes;
+import nl.hu.tosad.entities.domain.BusinessRule;
+import nl.hu.tosad.entities.domain.dto.AttributeRangeRuleDTO;
 import nl.hu.tosad.services.BusinessRuleServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.naming.OperationNotSupportedException;
 
 @Controller
 public class BusinessRuleController {
     @Autowired
     BusinessRuleServiceInterface businessRuleService;
 
+    @Autowired
+    BusinessRuleBuilderFactoryInterface businessRuleBuilderFactoryInterface;
+
     @GetMapping("/")
     public String test(Model model) {
+        model.addAttribute("businessRule", new AttributeRangeRuleDTO());
         model.addAttribute("businessRules", businessRuleService.getAllBusinessRules());
         return "test";
     }
+
+    @PostMapping("/addRule")
+    public String greetingSubmit(@ModelAttribute AttributeRangeRuleDTO businessRule) {
+        try {
+            BusinessRuleBuilder businessRuleBuilder = businessRuleBuilderFactoryInterface.getBuilder(BusinessRuleTypes.ATTRIBUTE_RANGE_RULE);
+            BusinessRule businessRule1 = businessRuleBuilder
+                    .setName(businessRule.getName())
+                    .setRange(businessRule.getRange())
+                    .build();
+            BusinessRule businessRule2 = businessRuleService.saveBusinessRule(businessRule1);
+            System.out.println(businessRule2);
+        } catch (OperationNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return "test";
+    }
+
 }
---
+++ src/main/java/nl/hu/tosad/entities/domain/dto/AttributeRangeRuleDTO.java
@@ -0,0 +1,22 @@
+package nl.hu.tosad.entities.domain.dto;
+
+public class AttributeRangeRuleDTO {
+    private String name;
+    private String range;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+}
