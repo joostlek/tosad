0840a7bae3d21ebf19f9dba0ecea3c595716ff01
joostlek <joostlek@outlook.com>
2019-01-15 15:24:46 +0100

    Move current module into a separate module Add second module for the generator
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ generator/src/main/java/nl/hu/generator/Main.java
@@ -0,0 +1,9 @@
+package nl.hu.generator;
+
+import nl.hu.tosad.entities.domain.AttributeRangeRule;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new AttributeRangeRule());
+    }
+}
