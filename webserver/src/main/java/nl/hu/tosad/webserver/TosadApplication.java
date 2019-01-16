5ee0c0f2bac37f85081f1db9b1caff58a167fd56
joostlek <joostlek@outlook.com>
2019-01-16 15:11:50 +0100

    Update test to also add a column
    Signed-off-by: joostlek <joostlek@outlook.com>

--- webserver/src/main/java/nl/hu/tosad/webserver/TosadApplication.java
+++ webserver/src/main/java/nl/hu/tosad/webserver/TosadApplication.java
@@ -1,7 +1,12 @@
 package nl.hu.tosad.webserver;
 
 import nl.hu.tosad.domain.entities.domain.AttributeRangeRule;
-import nl.hu.tosad.domain.entities.domain.BusinessRule;
+import nl.hu.tosad.domain.entities.domain.database.Database;
+import nl.hu.tosad.domain.entities.domain.database.DbColumn;
+import nl.hu.tosad.domain.entities.domain.database.DbTable;
+import nl.hu.tosad.webserver.repositories.ColumnRepository;
+import nl.hu.tosad.webserver.repositories.DatabaseRepository;
+import nl.hu.tosad.webserver.repositories.TableRepository;
 import nl.hu.tosad.webserver.services.BusinessRuleServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,13 +21,27 @@ public class TosadApplication implements CommandLineRunner {
     @Autowired
     private BusinessRuleServiceInterface businessRuleService;
 
+    @Autowired
+    private DatabaseRepository databaseRepository;
+
+    @Autowired
+    private ColumnRepository columnRepository;
+
+    @Autowired
+    private TableRepository tableRepository;
+
+
     public static void main(String[] args) {
         SpringApplication.run(TosadApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        BusinessRule businessRule = new AttributeRangeRule();
+        AttributeRangeRule businessRule = new AttributeRangeRule();
+        Database database = databaseRepository.save(new Database());
+        DbTable dbTable = tableRepository.save(new DbTable("asd", database));
+        DbColumn dbColumn = columnRepository.save(new DbColumn("asd", dbTable));
+        businessRule.setColumn(dbColumn);
         businessRule.setName("asd");
         businessRuleService.saveBusinessRule(businessRule);
     }
