ffea506f4951e9632e01ee8ca86a6a0ca85dd6bb
Niekham <Niekham@users.noreply.github.com>
2019-01-21 13:11:42 +0100

    Repository elk domeinklas

---
+++ webserver/src/main/java/nl/hu/tosad/webserver/business_ruletype/BusinessRuleTypeRepository.java
@@ -0,0 +1,9 @@
+package nl.hu.tosad.webserver.business_ruletype;
+
+import nl.hu.tosad.domain.ruletype.BusinessRuleType;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusinessRuleTypeRepository extends CrudRepository<BusinessRuleType, Long> {
+}
---
+++ webserver/src/main/java/nl/hu/tosad/webserver/business_ruletype/CategoryRepository.java
@@ -0,0 +1,9 @@
+package nl.hu.tosad.webserver.business_ruletype;
+
+import nl.hu.tosad.domain.ruletype.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
