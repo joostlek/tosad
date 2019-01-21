bf98734511a7c77e6b25244a70e972c2e5fdeb5f
ElineNube <31698656+ElineNube@users.noreply.github.com>
2019-01-21 11:41:41 +0100

    Made classes Operator, Template, Category

--- tosad/src/main/java/nl/hu/tosad/domain/ruletype/Operator.java
+++ tosad/src/main/java/nl/hu/tosad/domain/ruletype/Operator.java
@@ -1,4 +1,56 @@
 package nl.hu.tosad.domain.ruletype;
 
+import nl.hu.tosad.domain.rule.BusinessRule;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Operator {
+
+    public int id;
+    public String sign;
+
+    @ManyToMany
+    public List<BusinessRuleType> businesRuleTypes;
+
+    @ManyToMany
+    public List<BusinessRule> businessRules;
+
+    public Operator(int id, String sign) {
+        this.id = id;
+        this.sign = sign;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public List<BusinessRuleType> getBusinesRuleTypes() {
+        return businesRuleTypes;
+    }
+
+    public List<BusinessRule> getBusinessRules() {
+        return businessRules;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public void setBusinesRuleTypes(List<BusinessRuleType> businesRuleTypes) {
+        this.businesRuleTypes = businesRuleTypes;
+    }
+
+    public void setBusinessRules(List<BusinessRule> businessRules) {
+        this.businessRules = businessRules;
+    }
 }
