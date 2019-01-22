1c7ff59b1bed14567c637961885a3eb05deb90df
ElineNube <31698656+ElineNube@users.noreply.github.com>
2019-01-22 16:06:56 +0100

    Merge branch 'master' of C:\Users\eline\Documents\GitHub\tosad with conflicts.

--- webserver/src/main/java/nl/hu/tosad/webserver/rule/BusinessRuleController.java
+++ webserver/src/main/java/nl/hu/tosad/webserver/rule/BusinessRuleController.java
@@ -1,47 +1,38 @@
-//package nl.hu.tosad.webserver.rule;
-//
-//import nl.hu.tosad.domain.entities.domain.BusinessRule;
-//import nl.hu.tosad.domain.entities.domain.dto.AttributeRangeRuleDTO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//import javax.naming.OperationNotSupportedException;
-//
-//@Controller
-//public class BusinessRuleController {
-//    @Autowired
-//    BusinessRuleServiceInterface businessRuleService;
-//
+package nl.hu.tosad.webserver.rule;
+
+import nl.hu.tosad.webserver.ruletype.TemplateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BusinessRuleController {
+    @Autowired
+    BusinessRuleServiceInterface businessRuleService;
+
+    @Autowired
+    private TemplateRepository templateRepository;
+
 //    private BusinessRuleBuilderFactoryInterface builderFactoryInterface = new BusinessRuleBuilderFactory();
-//
-//    @GetMapping("/")
-//    public String ruleList(Model model) {
-//        model.addAttribute("businessRules", businessRuleService.getAllBusinessRules());
-//        return "ruleList";
-//    }
-//
-//    @GetMapping("/add")
-//    public String add(Model model) {
-//        model.addAttribute("businessRule", new AttributeRangeRuleDTO());
-//        return "add";
-//    }
-//
-//    @PostMapping("/addRule")
-//    public String greetingSubmit(@ModelAttribute AttributeRangeRuleDTO businessRule, Model model) throws OperationNotSupportedException {
-//        BusinessRuleBuilder businessRuleBuilder = builderFactoryInterface.getBuilder(BusinessRuleTypes.ATTRIBUTE_RANGE_RULE);
-//            BusinessRule businessRule1 = businessRuleBuilder
-//                    .setName(businessRule.getName())
-//                    .setRangeStart(businessRule.getRangeStart())
-//                    .setRangeEnd(businessRule.getRangeEnd())
-//                    .setOperator(businessRule.getOperator())
-//                    .build();
-//            BusinessRule businessRule2 = businessRuleService.saveBusinessRule(businessRule1);
-//            model.addAttribute("MadeBusinessRule", businessRule2);
-//            return "define_rule_succeed";
-//    }
-//
-//}
+
+    @GetMapping("/")
+    public String ruleList(Model model) {
+        model.addAttribute("businessRules", businessRuleService.getAllBusinessRules());
+        return "ruleList";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("templates", templateRepository.findAll());
+        return "add";
+    }
+
+    @PostMapping("/addType")
+    public String addType(Model model) {
+        model.addAttribute("type", new ChosenRuleTypeDTO());
+        return "fillRule";
+    }
+
+
+}
