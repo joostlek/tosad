2f749c1e14a9413fce23b4d2cf0ebca468a39cb6
Niekham <Niekham@users.noreply.github.com>
2019-01-22 14:58:30 +0100

    trigger templates zijn nog niet goed

--- generator/src/main/java/nl/hu/tosad/templates/staticData/EOTHtemplate.java
+++ generator/src/main/java/nl/hu/tosad/templates/staticData/EOTHtemplate.java
@@ -1,6 +1,22 @@
 package nl.hu.tosad.templates.staticData;
 
 public class EOTHtemplate {
-    String EOTHtemplate = "";
+    String EOTHtemplate = "CREATE OR REPLACE TRIGGER trg_eoth\n" +
+            "  before INSERT OR UPDATE\n" +
+            "  on medewerker\n" +
+            "  FOR EACH ROW\n" +
+            "  declare\n" +
+            "      v_waarde number;\n" +
+            "      e_exception exception;\n" +
+            "  begin\n" +
+            "    select count(*) into v_waarde from medewerker;\n" +
+            "    if (v_waarde > 5) THEN\n" +
+            "      raise e_exception;\n" +
+            "    END IF;\n" +
+            "\n" +
+            "    exception\n" +
+            "    when e_exception then\n" +
+            "    raise_application_error(-20000, 'Not possible bits');\n" +
+            "  end;";
 }
 
--- generator/src/main/java/nl/hu/tosad/templates/staticData/ICMPtemplate.java
+++ generator/src/main/java/nl/hu/tosad/templates/staticData/ICMPtemplate.java
@@ -1,5 +1,21 @@
 package nl.hu.tosad.templates.staticData;
 
 public class ICMPtemplate {
-    String ICMPtemplate = "";
+    String ICMPtemplate = "CREATE OR REPLACE TRIGGER trg_icmp\n" +
+            "  before INSERT OR UPDATE of STARTSALARIS\n" +
+            "  on contract\n" +
+            "  FOR EACH ROW\n" +
+            "  declare\n" +
+            "      v_waarde number;\n" +
+            "      e_exception exception;\n" +
+            "  begin\n" +
+            "    select SALARIS into v_waarde from medewerker where id = 1;\n" +
+            "    if (:NEW.STARTSALARIS < v_waarde) THEN\n" +
+            "      raise e_exception;\n" +
+            "    END IF;\n" +
+            "\n" +
+            "    exception\n" +
+            "    when e_exception then\n" +
+            "    raise_application_error(-20000, 'Not possible bits');\n" +
+            "  end;";
 }
