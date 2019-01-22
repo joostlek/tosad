c0434e280f29b14b012a0b80894717fbe91d00af
ElineNube <31698656+ElineNube@users.noreply.github.com>
2019-01-22 15:26:54 +0100

    Made DTO

--- webserver/src/main/java/nl/hu/tosad/webserver/rule/BusinessRuleController.java
+++ webserver/src/main/java/nl/hu/tosad/webserver/rule/BusinessRuleController.java
@@ -1,16 +1,10 @@
 package nl.hu.tosad.webserver.rule;
 
-import nl.hu.tosad.domain.rule.BusinessRule;
-import nl.hu.tosad.domain.ruletype.BusinessRuleType;
-import nl.hu.tosad.domain.target_database.Dialect;
 import nl.hu.tosad.webserver.ruletype.TemplateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.naming.OperationNotSupportedException;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class BusinessRuleController {
@@ -34,18 +28,12 @@ public class BusinessRuleController {
         return "add";
     }
 
-//    @PostMapping("/addRule")
-//    public String greetingSubmit(@ModelAttribute AttributeRangeRuleDTO businessRule, Model model) throws OperationNotSupportedException {
-//        BusinessRuleBuilder businessRuleBuilder = builderFactoryInterface.getBuilder(BusinessRuleTypes.ATTRIBUTE_RANGE_RULE);
-//            BusinessRule businessRule1 = businessRuleBuilder
-//                    .setName(businessRule.getName())
-//                    .setRangeStart(businessRule.getRangeStart())
-//                    .setRangeEnd(businessRule.getRangeEnd())
-//                    .setOperator(businessRule.getOperator())
-//                    .build();
-//            BusinessRule businessRule2 = businessRuleService.saveBusinessRule(businessRule1);
-//            model.addAttribute("MadeBusinessRule", businessRule2);
-//            return "define_rule_succeed";
-//    }
+    @PostMapping("/addType")
+    public String addType(Model model) {
+        model.addAttribute("type", new ChosenRuleTypeDTO());
+        return "fillRule";
+    }
+
+
 
 }
