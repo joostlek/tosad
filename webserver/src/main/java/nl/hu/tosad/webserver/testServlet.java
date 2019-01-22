3d28a3c0522138cbfeed332c5951b17acf7d83a5
Niekham <Niekham@users.noreply.github.com>
2019-01-22 21:11:27 +0100

    servlet

--- webserver/src/main/java/nl/hu/tosad/webserver/testServlet.java
+++ webserver/src/main/java/nl/hu/tosad/webserver/testServlet.java
@@ -7,13 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = {"/testServlet"})
+@WebServlet(urlPatterns = {"/"})
 public class testServlet extends HttpServlet {
-        public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        int value = Integer.parseInt(req.getParameter("v"));
-        int value2 = Integer.parseInt(req.getParameter("v1"));
+        public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int value1 = Integer.parseInt(req.getParameter("v1"));
+        int value2 = Integer.parseInt(req.getParameter("v2"));
+        int value3 = Integer.parseInt(req.getParameter("v3"));
 
         PrintWriter out = res.getWriter();
-        out.print(value + value2);
-    }
+        out.print(value1);
+        out.print(value2);
+        out.print(value3);
+        }
 }
