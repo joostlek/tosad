ce3aee9890db2566b351900f3a1458e77417d201
joostlek <joostlek@outlook.com>
2019-01-22 11:27:48 +0100

    Add BusinessRuleRepository
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ generator/src/main/java/nl/hu/tosad/generator/rule/BusinessRuleRepository.java
@@ -0,0 +1,9 @@
+package nl.hu.tosad.generator.rule;
+
+import nl.hu.tosad.domain.rule.BusinessRule;
+
+import java.util.List;
+
+public interface BusinessRuleRepository {
+    public List<BusinessRule> getBusinessRuleByList(List<Long> ids);
+}
