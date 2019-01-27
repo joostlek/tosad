7fa4388ce13c2c439a95dde42a66be2e4728fcce
Niekham <Niekham@users.noreply.github.com>
2019-01-27 22:03:24 +0100

    Create add database

--- webserver/src/main/java/nl/hu/tosad/webserver/target_database/data/DialectRepository.java
+++ webserver/src/main/java/nl/hu/tosad/webserver/target_database/data/DialectRepository.java
@@ -4,6 +4,9 @@ import nl.hu.tosad.domain.target_database.Dialect;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DialectRepository extends CrudRepository<Dialect, Long> {
+    List<Dialect> findAll();
 }
--- webserver/src/main/resources/static/js/define_rule.js
+++ webserver/src/main/resources/static/js/define_rule.js
@@ -1,1 +1,6 @@
 console.log("CONNECTED")
+
+
+function dbInSession(id){
+    console.log(id);
+}
