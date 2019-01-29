1c0f20d5711f80b0971790a7b5b1492aaf1fe816
Niekham <Niekham@users.noreply.github.com>
2019-01-29 07:47:54 +0100

    js part of alis

--- webserver/src/main/resources/static/js/define_rule.js
+++ webserver/src/main/resources/static/js/define_rule.js
@@ -1,6 +1,22 @@
-console.log("CONNECTED")
+// var i = -1;
+// function addToList(){
+//     var text = document.getElementById("vars").value,
+//         listNode = document.getElementById("list"),
+//         liNode = document.createElement("LI"),
+//         txtNode = document.createTextNode(text);
+//
+//     i++
+//     liNode.appendChild(txtNode);
+//     liNode.setAttribute("id", "liList")
+//     liNode.setAttribute("th:text", "${value["+i+"]}");
+//     listNode.appendChild(liNode);
+// }
 
+function addInput(){
+    var inputDiv = document.getElementById("newInputDiv"),
+        inputNode = document.createElement("input");
 
-function dbInSession(id){
-    console.log(id);
+    inputNode.setAttribute("th:field", "*{properties['__${attribute.key}__']}");
+    inputNode.setAttribute("type", "text");
+    inputDiv.appendChild(inputNode);
 }
